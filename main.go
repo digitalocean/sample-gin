a97d89e3e523d233ea4bbf5944e60e72b4497d59
Damanpreet Singh <daman.4880@gmail.com>
2025-12-02 16:26:33 +0000

    Add route /ping

--- main.go
+++ main.go
@@ -111,6 +111,19 @@ func setupRouter() *gin.Engine {
 		c.String(http.StatusOK, requestID.String())
 	})
 
+	r.GET("/ping", func(c *gin.Context) {
+		logRequest(c)
+		codeParams, ok := c.Request.URL.Query()["code"]
+		if ok && len(codeParams) > 0 {
+			statusCode, _ := strconv.Atoi(codeParams[0])
+			if statusCode >= 200 && statusCode < 600 {
+				c.Status(statusCode)
+			}
+		}
+		requestID := uuid.New()
+		c.String(http.StatusOK, requestID.String())
+	})
+
 	return r
 }
 
